c04ce4aef495dfbe0f01eb517a557d27135c9d56
colizmic <157726180+colizmic@users.noreply.github.com>
2024-02-06 13:21:43 -0500

    Create Lab5.ino

---
+++ Lab5/Lab5.ino
@@ -0,0 +1,36 @@
+//Michael Colizza
+// C++ code
+//
+//init. variables 
+int button1State = 0;
+int button2State = 0;
+
+void setup()
+{
+  //sets pinmodes 
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  
+}
+
+void loop()
+{
+  //set variables = output of buttons
+  int button1State = digitalRead(3);
+  int button2State = digitalRead(2);
+  
+//checks variable reading and sends power to LED
+  if (button1State == LOW){
+  	digitalWrite(13, HIGH);
+  }
+  else if (button2State == LOW){
+  	digitalWrite(13, HIGH);
+  }
+  else if (button2State == LOW && button1State == LOW){
+    digitalWrite(13, LOW);
+    }
+  else{
+  digitalWrite(13, LOW);
+  }
+}
